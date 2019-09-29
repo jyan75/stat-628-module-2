75fa4ffa38c69f821f6a76ba276fc9d54f281501
wenbo <wenbofei@gmail.com>
2019-09-29 12:40:50 -0500

    commit0929

--- ModelSele.R
+++ ModelSele.R
@@ -1,3 +1,6 @@
+bodyfat.dat=read.csv('cleanfile.csv')
+View(bodyfat.dat)
+
 library(leaps)
 #function definitions
 #my.regsub selects variables using aic and cp
@@ -6,19 +9,20 @@ my.regsub <- function(matrix,y,nbest,method,nvmax=8){
   p<-ncol(matrix)+1
   temp <- regsubsets(matrix,y,nbest=nbest,method=method,nvmax=nvmax)
   temp.mat <- cbind(summary(temp)$which,summary(temp)$cp,
-                    summary(temp)$bic+p*(2-log(n)))
+                    summary(temp)$bic+p*(2-log(n)), summary(temp)$bic)
   dimnames(temp.mat)[[2]] <- c(dimnames(summary(temp)$which)[[2]],
-                                "cp", "aic")
+                               "cp", "aic", "bic")
   return(temp.mat)
 }
 
-bodyfat.dat=read.csv('cleanfile.csv')
-lm.mod1 <- lm(BODYFAT ~ .,data=subset(bodyfat.dat,select = -DENSITY))
+# lm.mod1 <- lm(BODYFAT ~ .,data=subset(bodyfat.dat,select = -DENSITY))
+# step(lm.mod1, direction = "both", k = 2)
 
 regsub.mat<-my.regsub(bodyfat.dat[,-c(1,2)],bodyfat.dat$BODYFAT,nbest=1,method = "exhaustive")
 regsub.mat[which.min(regsub.mat[,"aic"]),]
 #Keep WEIGHT, ABDOMEN, WRIST under AIC criteron
 regsub.mat[which.min(regsub.mat[,"cp"]),]
 #Keep AGE, HEIGHT, NECK, CHEST, ABDOMEN, FOREARM, RIST under Mallow's Cp criteron
+regsub.mat[which.min(regsub.mat[,"bic"]),]
+#Keep WEIGHT, ABDOMEN, WRIST under BIC criteron
 
-step(lm.mod1, direction = "both", k = 2)
---
+++ RegDiag.R
@@ -0,0 +1,37 @@
+
+#I choose to include  WEIGHT, ABDOMEN, WRIST which was choosen by both AIC and BIC criteria, and
+#satisfy the rule of thumb
+#regression diagnostic
+final.model <- lm(BODYFAT ~ WEIGHT+ABDOMEN+WRIST,data=bodyfat.dat)
+summary(final.model)
+par(mfrow = c(2,3))
+plot(final.model, which = 1:6)
+
+#Independence:Assume independence as we collect the data on randomly chosen individual
+
+
+##residual plots: no non-linear trend, can be considered as equal variance
+
+##Response Normality:
+MASS::boxcox(bodyfat.dat$BODYFAT ~ bodyfat.dat$WEIGHT+bodyfat.dat$ABDOMEN+bodyfat.dat$WRIST)
+#lambda=1 is in the 95% confidence interval, so we don't do transformation to fit normality
+#although the upper tail and power tail of qqplot is not good
+
+##see whether has outlier(y too large or too small)
+which(abs(rstudent(final.model))>2.576) # 2.576 is the 0.01 quantial of N(0,1)
+which(abs(rstandard(final.model))>2.576)
+car::outlierTest(final.model)
+#no outlier
+
+##see whether has high leverage point(x as a outlier with h too large)
+(2*4/dim(bodyfat.dat)[1]) # 2p/n
+unname(which(hatvalues(final.model)>0.03252033) )
+#high leverage point doesn't mean outlier, we don't delete
+
+##see high influential point(Cook's distance large)
+4/(dim(bodyfat.dat)[1]-4) #4/n-p
+unname(which(cooks.distance(final.model)>0.01652893))
+unname(which(cooks.distance(final.model)>0.5))
+#high leverage point doesn't mean outlier, we don't delete
+
+#or just influence.measures(final.model)
