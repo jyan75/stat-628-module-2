fd9d0d1d57aba0960fe8520ac2ad968fba7a9e73
jyan75 <jyan75@wisc.edu>
2019-10-03 13:06:26 -0500

    Update ModelSele.R
    add variable selection by R square

--- ModelSele.R
+++ ModelSele.R
@@ -9,7 +9,9 @@ my.regsub <- function(matrix,y,nbest,method,nvmax=8){
   p<-ncol(matrix)+1
   temp <- regsubsets(matrix,y,nbest=nbest,method=method,nvmax=nvmax)
   temp.mat <- cbind(summary(temp)$which,summary(temp)$cp,
-                    n*log(2*pi)+n*log(summary(temp)$rss/(n-(2:(nvmax+1))))+n+(2:(nvmax+1))+2,summary(temp)$bic)
+                    n*log(2*pi)+n*log(summary(temp)$rss/(n-(2:(nvmax+1))))+n+(2:(nvmax+1))+2,
+                    summary(temp)$bic,
+                    1-summary(temp)$rss/sum((y-mean(y))^2))
   dimnames(temp.mat)[[2]] <- c(dimnames(summary(temp)$which)[[2]],
                                "cp", "aic", "bic_drop")
   return(temp.mat)
@@ -25,4 +27,9 @@ regsub.mat[which.min(regsub.mat[,"cp"]),]
 #Keep AGE, HEIGHT, NECK, CHEST, ABDOMEN, FOREARM, WRIST under Mallow's Cp criteron
 regsub.mat[which.min(regsub.mat[,"bic_drop"]),]
 #Keep WEIGHT, ABDOMEN, WRIST under BIC criteron
+plot(regsub.mat[,"r_square"],type="b",
+     main=bquote("Variable selection by"~R^2),
+     xlab="Number of variables",
+     ylab=expression(R^2)
+     )
 
